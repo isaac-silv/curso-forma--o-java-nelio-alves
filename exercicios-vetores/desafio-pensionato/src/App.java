daae3296b0b1d3473288ab3f3ee2b702d8e66e43
isaac-silv <isaacaquino.dv@gmail.com>
2023-05-08 17:32:54 -0300

    Resolução do desafio pensionato

---
+++ exercicios-vetores/desafio-pensionato/src/App.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+import models.Rent;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many rooms will be rented? ");
+        int n = sc.nextInt();
+        Rent[] rents = new Rent[10];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Rent #" + (i + 1) + ":");
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.nextLine();
+            System.out.print("Room: ");
+            int room = sc.nextInt();
+
+            rents[room] = new Rent(name, email);
+        }
+
+        System.out.println("Budy rooms:");
+        for (int i = 0; i < rents.length; i++) {
+            if (rents[i] != null) {
+                System.out.println(i + ": " + rents[i].getName() + ", " + rents[i].getEmail());
+            }
+        }
+    }
+}
